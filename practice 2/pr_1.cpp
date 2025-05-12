4a8fd44fbf7d81608542e69fd274686eebfc8919
Gautam Gohil <gohilgautam2405@gmail.com>
2025-05-12 11:38:37 +0530

    practice 2

---
+++ practice 2/pr_1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    int a = 15, b = 20;
+    
+    // for sum 
+    cout << "addition of " << a << " and " << b << " is "  << a + b << endl;
+
+    // for subtraction 
+    cout << "subtraction of " << a << " and " << b << " is "  << a - b << endl;
+
+    // for Multiplication 
+    cout << "Multiplication  of " << a << " and " << b << " is "  << a * b << endl;
+
+    // for Division  
+    cout << "Division of " << a << " and " << b << " is "  << a / b << endl;
+    // for Modulo  
+    cout << "Modulo of " << a << " and " << b << " is "  << a % b << endl << endl << endl;
+
+    cout <<  a << " + " << b << " = "  << a + b << endl;
+    cout <<  a << " - " << b << " = "  << a - b << endl;
+    cout <<  a << " * " << b << " = "  << a * b << endl;
+    cout <<  a << " / " << b << " = "  << a / b << endl;
+    cout <<  a << " % " << b << " = "  << a % b << endl;
+
+
+}
