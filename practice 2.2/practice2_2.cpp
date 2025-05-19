ae0db3141de415cca6cd91f41f69b2ca73846de9
Gautam Gohil <gohilgautam2405@gmail.com>
2025-05-19 12:11:05 +0530

    practice 2.2

---
+++ practice 2.2/practice2_2.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+    //  Write a Program to find the area of a circle.
+    // float r, area;
+    // cout << "Enter the radius of the circle: ";
+    // cin >> r;
+    // area = 3.14 * r * r;
+    // cout << "The area of the circle is: " << area;
+
+    // Write a Program to find the area of a rectangle.
+
+    // float l, b, area;
+    // cout << "Enter the length of the rectangle: ";
+    // cin >> l;
+    // cout << "Enter the breadth of the rectangle: ";
+    // cin >> b;
+    // area = l * b;
+    // cout << "The area of the rectangle is: " << area;
+    
+
+    //Write a Program to find the area of a triangle.
+    
+    // float b, h, area;
+    // cout << "Enter the base of the triangle: ";
+    // cin >> b;
+    // cout << "Enter the height of the triangle: ";
+    // cin >> h;   
+    // area = 0.5 * b * h;
+    // cout << "The area of the triangle is: " << area;
+    
+
+    // Q.4 Write a Program to find Simple interest.
+
+    // float p, r, t, si;
+    // cout << "Enter the principal amount: ";
+    // cin >> p;
+    // cout << "Enter the rate of interest: ";
+    // cin >> r;
+    // cout << "Enter the time period: ";
+    // cin >> t;
+    // si = (p * r * t) / 100;
+    // cout << "The simple interest is: " << si;
+    
+    
+    // Q.5 Write a Program to find the Perimeter of the circle
+
+    float r, p;
+    cout << "Enter the radius of the circle: ";
+    cin >> r;
+    p = 2 * 3.14 * r;
+    cout << "The perimeter of the circle is: " << p;
+}
